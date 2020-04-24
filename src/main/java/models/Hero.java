4c03fb859d6388566ab866fc58712526620f0089
rmogusu <rmogusu123@gmail>
2020-04-25 00:32:50 +0200

    Written a coded logic to make sure my test for creating name successfully pass.

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,8 +1,12 @@
 package models;
 
 public class Hero {
+    private String name;
     public Hero(String name , int age, String power, String weakness) {
+        this.name=name;
     }
-
+    public String getName() {
+        return  name;
+    }
 
 }
