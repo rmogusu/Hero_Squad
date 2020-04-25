ab23685e43dc83738cf94911aa4f44af8ec5786e
rmogusu <rmogusu123@gmail>
2020-04-25 23:12:36 +0200

    Create my database test file and set all connections.

---
+++ src/test/java/dao/Sql2oHeroDaoTest.java
@@ -0,0 +1,24 @@
+package dao;
+
+import models.Hero;
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class Sql2oHeroDaoTest {
+    private Sql2oHeroDao heroDao;
+    private Connection conn;
+
+    @Before
+    public void setUp() throws Exception {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "rose", "wambua");
+        heroDao = new Sql2oHeroDao(sql2o);
+        conn = sql2o.open();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        conn.close();
+    }
+}
