b133c1bcd4746993e411822dc626b84787fb676f
rmogusu <rmogusu123@gmail>
2020-04-25 13:20:55 +0200

    Write a dummy methode that will ruturn all Heroes.

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,10 +1,13 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Hero {
     private String name;
     private int   age;
     private  String power;
     private  String weakness;
+    private static ArrayList<Hero> instances = new ArrayList<>();
     public Hero(String name , int age, String power, String weakness) {
         this.name=name;
         this.age =age;
@@ -23,5 +26,11 @@ public class Hero {
     public String getWeakness(){
         return weakness;
     }
+    public static ArrayList<Hero> getAll(){
+        return instances;
+    }
 
+    public static void clearAllHeroes(){
+        instances.clear();
+    }
 }
