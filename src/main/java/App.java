4f7ce54ab04dd153164f443638069a1a35b22a00
rmogusu <rmogusu123@gmail>
2020-04-25 12:57:51 +0200

    Create  root  route in app.java file that will render my home page.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,18 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+import static spark.Spark.*;
 import static spark.Spark.staticFileLocation;
 
 public class App {
-    public static void main(String[] args) { //type “psvm + tab” to autocreate this :)
+    public static void main(String[] args) {
         staticFileLocation("/public");
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
 
     }
 }
