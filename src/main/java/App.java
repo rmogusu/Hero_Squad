4616c0a4c74a10c61856a69e0314f280d8f5688e
rmogusu <rmogusu123@gmail>
2020-04-25 16:22:14 +0200

    Add a root route to be able to ccess my form.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,10 +9,15 @@ import static spark.Spark.staticFileLocation;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+        //get: show home page
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
-
+        //get: show new Heroes form
+        get("/heroes/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "hero-form.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
