2ac4aa7e3e93f10ca85d74972d5f28c6089a231a
rmogusu <rmogusu123@gmail>
2020-04-25 23:18:31 +0200

    Add interface class to implement all required methods by my application.

---
+++ src/main/java/dao/HeroDao.java
@@ -0,0 +1,23 @@
+package dao;
+import models.Hero;
+import java.util.List;
+public interface HeroDao {
+
+    // LIST
+    List<Hero> getAll();
+
+    // CREATE
+    void add(Hero hero);
+
+    // READ
+    Hero findById(int id);
+
+    // UPDATE
+    //void update(int id, String name,int age,String power, String weakness);
+
+    // DELETE
+
+    //void deleteById(int id);
+    //void clearAllHeroes();
+}
+
