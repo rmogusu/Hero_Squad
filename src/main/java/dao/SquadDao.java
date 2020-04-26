015167c9dd1ec0f0cf61d3fbc706243672ae9bc0
rmogusu <rmogusu123@gmail>
2020-04-26 14:40:31 +0200

    Add a class interface to squad class implementing all methods required to it.

---
+++ src/main/java/dao/SquadDao.java
@@ -0,0 +1,26 @@
+package dao;
+
+import models.Hero;
+import models.Squad;
+
+import java.util.List;
+
+public interface SquadDao {
+
+    // LIST
+    List<Squad> getAll();
+
+    // CREATE
+    void add(Squad squad);
+
+    // READ
+    Squad findById(int id);
+
+    // UPDATE
+    //void update(int id, String name,int max_size,String cause);
+
+    // DELETE
+
+    //void deleteById(int id);
+    //void clearAllSquads();
+}
