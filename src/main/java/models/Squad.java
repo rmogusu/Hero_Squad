f3b4d42a27c988f3e3b838bf49725994d213b1c7
rmogusu <rmogusu123@gmail>
2020-04-26 13:33:47 +0200

    Create new constructor in squad class with the required arguments.

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,4 +1,13 @@
 package models;
 
 public class Squad {
+    private String name;
+    private String cause;
+    private int max_size;
+    private int id;
+    public Squad (String name ,  String cause ,int max_size) {
+        this.name=name;
+        this.cause =cause;
+        this.max_size=max_size;
+    }
 }
