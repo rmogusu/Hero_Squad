55f5a728905b3d768851c1608ec9a1c37b3fe33b
rmogusu <rmogusu123@gmail>
2020-04-26 13:54:25 +0200

    Add the required tests for squad class and make sure they pass.

---
+++ src/test/java/models/SquadTest.java
@@ -0,0 +1,44 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SquadTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    @Test
+    public void NewSquadObjectGetsCorrectlyCreated_true() throws Exception {
+        Squad squad  = setupNewSquad();
+        assertEquals(true, squad instanceof Squad);
+    }
+    @Test
+    public void Squad_InstantiatesWithName_true() throws Exception {
+        Squad squad  = setupNewSquad();
+        assertEquals("Super", squad.getName());
+    }
+    @Test
+    public void Squad_InstantiatesWithMax_Size_true() throws Exception {
+        Squad squad  = setupNewSquad();
+        assertEquals(15, squad.getMax_size());
+    }
+    @Test
+    public void Squad_InstantiatesWithCause_true() throws Exception {
+        Squad squad  = setupNewSquad();
+        assertEquals("Fight sexism", squad.getCause());
+    }
+
+
+    //helper methods
+    public Squad setupNewSquad(){
+        return new Squad  ("Super","Fight sexism",15);
+    }
+}
