9a1ea4d5bfddd9424fdec9f8dd7b3b2badb8c0a4
rmogusu <rmogusu123@gmail>
2020-04-26 13:37:16 +0200

    Generate getter and setter methods for Squad class.

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -10,4 +10,36 @@ public class Squad {
         this.cause =cause;
         this.max_size=max_size;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public int getMax_size() {
+        return max_size;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCause(String cause) {
+        this.cause = cause;
+    }
+
+    public void setMax_size(int max_size) {
+        this.max_size = max_size;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
